40be5710540f9307ab9ca056ab46dad83efc1b3a
Khang <khangb612@gmail.com>
2020-03-04 14:09:19 +1100

    [Update] Week 3 material completed

--- app/src/main/java/com/example/mobileapplicationweek2/DetailActivity.java
+++ app/src/main/java/com/example/mobileapplicationweek2/DetailActivity.java
@@ -6,36 +6,71 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
-    private TextView mDetailMessage;
-    private Button mShowVideoButton;
+    private TextView coinLong;
+    private TextView coinShort;
+    private TextView value;
+    private TextView hourChange;
+    private TextView dayChange;
+    private TextView weekChange;
+    private TextView market;
+    private TextView volume;
+    private ImageView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        final Intent intent = getIntent();
+        final String symbol = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        Coin coin = Coin.searchCoin(symbol);
 
-        mDetailMessage = findViewById(R.id.tvdetailMessage);
-        mDetailMessage.setText(message);
+        coinLong = (TextView) findViewById(R.id.mCoinLong);
+        coinShort = (TextView) findViewById(R.id.mCoinShort);
+        value = (TextView) findViewById(R.id.mValue);
+        hourChange = (TextView) findViewById(R.id.mHourChange);
+        dayChange = (TextView) findViewById(R.id.mDayChange);
+        weekChange = (TextView) findViewById(R.id.mWeekChange);
+        market = (TextView) findViewById(R.id.mMarket);
+        volume = (TextView) findViewById(R.id.mVolume);
+        search = (ImageView) findViewById(R.id.mSearch);
 
-        mShowVideoButton = findViewById(R.id.btnShowVideo);
-        mShowVideoButton.setOnClickListener(new View.OnClickListener() {
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showVideo("https://www.youtube.com/watch?v=kOHB85vDuow");
+
             }
         });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search(symbol);
+            }
+        });
+
+        showCoin(coin);
     }
 
-    private void showVideo(String url) {
+    private void showCoin(Coin coin) {
+        coinLong.setText(coin.getName());
+        coinShort.setText(coin.getSymbol());
+        value.setText(String.valueOf(coin.getValue()));
+        hourChange.setText(String.valueOf(coin.getChange1h()));
+        dayChange.setText(String.valueOf(coin.getChange24h()));
+        weekChange.setText(String.valueOf(coin.getChange7d()));
+        market.setText(String.valueOf(coin.getMarketcap()));
+        volume.setText(String.valueOf(coin.getVolume()));
+    }
+
+    private void search(String query) {
+        String url = "https://www.google.com/?q="+query;
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/mobileapplicationweek2/MainActivity.java
+++ app/src/main/java/com/example/mobileapplicationweek2/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchDetailActivity("Hello from DetailActivity ***");
+                launchDetailActivity("BTC");
             }
         });
     }
