cf39fd96f50e523a9b8687ca2b14e63fb8371a98
Khang <khangb612@gmail.com>
2020-04-08 13:12:42 +1000

    feat: Week 8 - MainActivity and DetailFragment is realtime

--- app/src/main/java/com/example/mobileapplicationweek2/DetailFragment.java
+++ app/src/main/java/com/example/mobileapplicationweek2/DetailFragment.java
@@ -16,7 +16,14 @@ import com.example.mobileapplicationweek2.Entites.Coin;
 import com.example.mobileapplicationweek2.Entites.CoinLoreResponse;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import static com.example.mobileapplicationweek2.Entites.CoinLoreResponse.json;
 
@@ -44,11 +51,6 @@ public class DetailFragment extends Fragment {
         Bundle arguments = getArguments();
         int position = arguments.getInt("POSITION");
 
-        Gson gson = new Gson();
-        CoinLoreResponse response = gson.fromJson(json, CoinLoreResponse.class);
-        List<Coin> list = response.getData();
-        final Coin coin = list.get(position);
-
         coinLong = v.findViewById(R.id.mCoinLong);
         coinShort = v.findViewById(R.id.mCoinShort);
         value = v.findViewById(R.id.mValue);
@@ -59,27 +61,56 @@ public class DetailFragment extends Fragment {
         volume = v.findViewById(R.id.mVolume);
         search = v.findViewById(R.id.mSearch);
 
-        coinLong.setText(coin.getName());
-        coinShort.setText(coin.getSymbol());
-        value.setText("$" + coin.getPriceUsd());
-        hourChange.setText(coin.getPercentChange1h() + " %");
-        dayChange.setText(coin.getPercentChange24h() + " %");
-        weekChange.setText(coin.getPercentChange7d() + " %");
-        market.setText("$" + String.format("%,.2f", Double.parseDouble(coin.getMarketCapUsd())));
-        volume.setText("$" + String.format("%,.2f", coin.getVolume24()));
+        //Try-catch ensures any connection problems wont cause app to crash
+        try {
+            //Retrofit is used to get online APIs
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://api.coinlore.net/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            CoinService service = retrofit.create(CoinService.class);
+            Call<CoinLoreResponse> coinsCall = service.getCoins();
 
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            //Enqueue is used to manage Asynchronous responses
+            coinsCall.enqueue(new Callback<CoinLoreResponse>() {
+                @Override
+                public void onResponse(Call<CoinLoreResponse> call, Response<CoinLoreResponse> response) {
+                    if (response.isSuccessful()) {
+                        //Updates coin list
+                        List<Coin> list = response.body().getData();
+                        Coin coin = list.get(position);
+                        coinLong.setText(coin.getName());
+                        coinShort.setText(coin.getSymbol());
+                        value.setText("$" + coin.getPriceUsd());
+                        hourChange.setText(coin.getPercentChange1h() + " %");
+                        dayChange.setText(coin.getPercentChange24h() + " %");
+                        weekChange.setText(coin.getPercentChange7d() + " %");
+                        market.setText("$" + String.format("%,.2f", Double.parseDouble(coin.getMarketCapUsd())));
+                        volume.setText("$" + String.format("%,.2f", coin.getVolume24()));
 
-            }
-        });
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                search(coin.getName());
-            }
-        });
+                        search.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        });
+                        search.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                search(coin.getName());
+                            }
+                        });
+                    }
+                    else {
+                    }
+                }
+                @Override
+                public void onFailure(Call<CoinLoreResponse> call, Throwable t) {
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return v;
     }
